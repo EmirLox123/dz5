8366340519eed7683df4d084e5c83f77a113d12d
ShutkaEmirKrut123 <126079644+EmirLox123@users.noreply.github.com>
2023-03-15 16:13:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import colorama
-data = "string"
-print(getattr(data, "startswith"))
-print(getattr(data, "startswith", None))
-print(getattr(data, "reverse", None))
+from colorama import init, Fore, Back, Style
+
+# инициализация библиотеки
+init()
+
+# пример использования атрибутов Fore и Back
+print(Fore.RED + 'Красный текст' + Fore.RESET)
+print(Back.GREEN + 'Зеленый фон' + Back.RESET)
+
+# пример использования атрибута Style
+print(Style.DIM + 'Текст с затемнением' + Style.RESET_ALL)
+
+# пример использования нескольких атрибутов одновременно
+print(Fore.YELLOW + Back.BLUE + Style.BRIGHT + 'Желтый текст на синем фоне с ярким стилем' + Style.RESET_ALL
